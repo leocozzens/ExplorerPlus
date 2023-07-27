783f72b25054f7de582531091dcf2ac519c28011
Leo Cozzens <leo@narthexnetworks.com>
2023-07-26 19:31:04 -0500

    Fixed windows build script

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
-REM ExplorerPlus build script
+@REM ExplorerPlus build script
+@REM Use "ALL" to compile with libraries
 @ECHO OFF
 SetLocal EnableDelayedExpansion
 
@@ -52,20 +53,23 @@ SET LFLAGS=-lopengl32 -lgdi32
 ECHO Building %PROJECT% %RELEASE%
 
 FOR %%f IN (%BASELIBS%) DO (
-    IF "%1"=="" (
-        goto :SKIPLIB
+    IF NOT "%1"=="" (
+        IF NOT "%1"=="NOLIB" (
+            ECHO Compiling lib %%~nf
+            %CC% %CFLAGS% -c %IFLAGS% -I%BASELIB%/internal "%%f" -o "%LIBOBJ%/%%~nf.o"
+        )
     )
-    ECHO Compiling lib %%~nf
-    %CC% %CFLAGS% -c %IFLAGS% -I%BASELIB%/internal "%%f" -o "%LIBOBJ%/%%~nf.o"
     SET "LIBOBJS=!LIBOBJS! %LIBOBJ%/%%~nf.o"
 )
-:SKIPLIB
 
 FOR %%f IN (%SRCS%) DO (
     IF NOT "%1"=="" (
         IF NOT "%1"=="ALL" (
-            IF NOT "%1"=="%%~nf" (
-                goto :BREAK
+            IF NOT "%1"=="NOLIB" (
+                IF NOT "%1"=="%%~nf" (
+                    ECHO "%1" is not a valid source file
+                    EXIT /B 0 
+                )
             )
         )
     )
