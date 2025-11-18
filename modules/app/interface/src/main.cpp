8a426780d7569759a82031551b31fd8cf3852404
Leo Cozzens <cozzensleo@gmail.com>
2025-11-17 18:26:42 -0600

    Base rewrite

---
+++ modules/app/interface/src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include <wx/wx.h>
+
+class MyApp : public wxApp {
+public:
+    bool OnInit() override {
+        auto* frame = new wxFrame(nullptr, wxID_ANY, "Static wxWidgets");
+        frame->Show();
+        return true;
+    }
+};
+
+
+#ifdef RELEASE_BUILD
+wxIMPLEMENT_APP(MyApp);
+#else
+wxIMPLEMENT_APP_CONSOLE(MyApp);
+#endif
